f292e34c95cd0e25401c779fb05274985d85a3d0
whydigo <ukunsss7@gmail.com>
2023-01-19 17:12:40 +0300

    возня с роутами

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import SignIn from "./components/Authorization/SignIn";
 import SignUp from "./components/Authorization/SignUp";
 import Service from "./components/Services/Service";
 import Header from "./components/Header/Header";
-
+import ServiceSaleRule from "./components/Services/ServicesItems/ServiceSaleRule";
+import ServiceLocation from "./components/Services/ServicesItems/ServiceLocation";
+import ServiceReturn from "./components/Services/ServicesItems/ServiceReturn";
+import ServiceRefund from "./components/Services/ServicesItems/ServiceRefund";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/services" element={<Service />} />
+
+        <Route
+          path="/services/pravila-prodazhi"
+          element={<ServiceSaleRule />}
+        />
+        <Route path="/services/location" element={<ServiceLocation />} />
+        <Route path="/services/vozvrat-tovara" element={<ServiceReturn />} />
+        <Route path="/services/vozvrat-denezhnyh-sredstv" element={<ServiceRefund />} />
       </Routes>
     </div>
   );
