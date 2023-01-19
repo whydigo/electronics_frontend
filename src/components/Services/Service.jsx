0c75e8a8a798879ead237b588337cb98c3696754
UmarUmarov1 <116254464+UmarUmarov1@users.noreply.github.com>
2023-01-19 17:35:34 +0300

    Добавлен Реквизиты

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ServiceLocation from "./ServicesItems/ServiceLocation";
 import ServiceOrder from "./ServicesItems/ServiceOrder";
+import Requisites from "./ServicesItems/Requisites";
 
 const Service = () => {
   return (
     <div>
       <ServiceOrder />
       <ServiceLocation />
+      <Requisites />
     </div>
   );
 };
