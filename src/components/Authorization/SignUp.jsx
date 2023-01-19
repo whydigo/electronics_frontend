d15fef101af6be63a2009a08b451d9df5ec08618
Dovakin95 <Nepstor95@gamil.com>
2023-01-19 14:32:49 +0300

    добавлена регистрация

--- src/components/Authorization/SignUp.jsx
+++ src/components/Authorization/SignUp.jsx
@@ -6,6 +6,7 @@ import '../../styles/auth.css'
 import {Link} from 'react-router-dom'
 
 
+
 const SignUp = () => {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
@@ -28,7 +29,7 @@ const SignUp = () => {
   };
   return (
    <>
-   
+    
     <div className="auth__main">
       <form className="auth__form" onSubmit={handleSignUp}>
         <input
@@ -51,11 +52,7 @@ const SignUp = () => {
         <br />
         <Link className="authLink" to='/login'>Войти</Link>
       </form>
-      <div className="auth__elements">
-        <div className="auth__element_4"></div>
-        <div className="auth__element_5"></div>
-        <div className="auth__element_6"></div>
-      </div>
+    
     </div>
     </>
   );
