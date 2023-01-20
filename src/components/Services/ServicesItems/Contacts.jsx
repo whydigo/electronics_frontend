f8eb5977a24fac9ecfd5ad42acf0657491b36cd1
Sharif-SSAT <sharip16takaev16@gmail.com>
2023-01-20 10:44:21 +0300

    немного работы

---
+++ src/components/Services/ServicesItems/Contacts.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../../styles/contacts.css';
+
+const Contacts = () => {
+    return (
+        <div className='wrapper'>
+            <div className='c-head'>
+                <h1 className='c-zagolovok'>Контакты</h1>
+            </div>
+            <div className='c-main'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Contacts;
