139b6def8a2195ee812037374fd1ea02b55b7d64
Islam <amg_95@inbox.ru>
2023-01-20 11:14:57 +0300

    сделана страница о нас

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from "./components/Authorization/SignIn";
 import SignUp from "./components/Authorization/SignUp";
 import Service from "./components/Services/Service";
 import Header from "./components/Header/Header";
+import AboutUs from "./components/Services/ServicesItems/AboutUs";
 
 
 function App() {
@@ -14,9 +15,12 @@ function App() {
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/services" element={<Service />} />
+		  <Route path="/o-nas" element={<AboutUs/>}/>
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+ 
