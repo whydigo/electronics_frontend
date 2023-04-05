7ffb401443644105bca98be4428620f1046d5461
whydigo <ukunsss7@gmail.com>
2023-04-05 18:19:11 +0300

    создан компонент с универсальным модальным окном

--- src/components/Cart/Product/Review.jsx
+++ src/components/Cart/Product/Review.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { fetchReviews } from "../../../features/ReviewSlice";
 import s from "../../../styles/Review.module.css";
+import Modal from "../../Modal/Modal";
 
 const Review = ({ id, name, image }) => {
+  const [modalActive, setModalActive] = useState(false);
   const reviews = useSelector((state) => state.reviewReducer.reviews);
   const filteredRev = reviews.filter((i) => i.product === id);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchReviews());
   }, [dispatch]);
-
   return (
     <div className={s.reviews__container}>
-      <div className={s.title}>Отзывы</div>
+      <div className={s.reviews__header}>
+        <div className={s.title}>
+          {filteredRev.length !== 0 ? "Отзывы" : "Отзывов нет"}
+        </div>
+        <div className={s.add} onClick={() => setModalActive(true)}>
+          Оставить отзыв
+        </div>
+      </div>
       <div className={s.reviews}>
         <Swiper
           className={s.swiper}
           modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={10}
+          spaceBetween={40}
           slidesPerView={3}
           loop={true}
         >
@@ -36,6 +44,13 @@ const Review = ({ id, name, image }) => {
           })}
         </Swiper>
       </div>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <div className={s.modalTitle}>Ваша критика очень важна для нас</div>
+        <form className={s.modalForm} onClick={(e) => e.preventDefault()}>
+          <input className={s.modalInput} type="text" />
+          <button className={s.modalBtn}>Готово</button>
+        </form>
+      </Modal>
     </div>
   );
 };
