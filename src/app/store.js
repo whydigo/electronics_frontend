653653d6e739741a3164886c4eccedae4738c57d
whydigo <ukunsss7@gmail.com>
2023-04-05 15:25:40 +0300

    создан компонент для отзывов и налажен их вывод на страинцу

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/applicationSlice";
 import categoryReducer from "../features/CategoriesSlice";
 import productReducer from "../features/ProductSlice";
+import reviewReducer from "../features/ReviewSlice";
 
 export const store = configureStore({
   reducer: {
     application,
     categoryReducer,
     productReducer,
+    reviewReducer,
   },
 });
---
+++ src/features/ReviewSlice.js
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  reviews: [],
+  error: null,
+  loading: false,
+};
+
+export const fetchReviews = createAsyncThunk(
+  "reviews/fetch",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:4000/review");
+      const reviews = await res.json();
+
+      if (reviews.error) {
+        return thunkAPI.rejectWithValue(reviews.error);
+      }
+      return thunkAPI.fulfillWithValue(reviews);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const postProduct = createAsyncThunk(
+  "reviews/post",
+  async (data, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:4000/review", {
+        method: "POST",
+        body: JSON.stringify({ text: data.text, product: data.product }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const reviews = await res.json();
+      return reviews;
+    } catch (error) {
+        thunkAPI.rejectWithValue(error.message)
+    }
+  }
+);
+
+const ReviewsSlice = createSlice({
+  name: "reviews",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchReviews.fulfilled, (state, action) => {
+        state.reviews = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchReviews.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchReviews.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      });
+  },
+});
+
+export default ReviewsSlice.reducer;
